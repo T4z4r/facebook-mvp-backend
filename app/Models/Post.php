f85b6b958494fbc112f45e8e9447a4a0d34dedc3
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 09:07:17 +0300

    fix: post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,24 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['user_id', 'content', 'image_url'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
