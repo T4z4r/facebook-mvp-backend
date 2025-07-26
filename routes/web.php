46c8d2acd0fe627f40dc7d83ab0c60cb5c37b039
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 10:00:59 +0300

    fix: web routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,49 @@
 <?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\GroupPostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/password/reset', [AuthController::class, 'showResetForm'])->name('password.request');
+Route::post('/password/reset', [AuthController::class, 'resetPassword'])->name('password.email');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [PostController::class, 'index'])->name('home');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+
+    Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('likes.store');
+    Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('likes.destroy');
+
+    Route::get('/friends', [FriendController::class, 'index'])->name('friends.index');
+    Route::get('/friends/requests', [FriendController::class, 'requests'])->name('friends.requests');
+    Route::post('/friends', [FriendController::class, 'store'])->name('friends.store');
+    Route::put('/friends/{friend}/accept', [FriendController::class, 'accept'])->name('friends.accept');
+
+    Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::post('/messages', [MessageController::class, 'store'])->name('messages.store');
+
+    Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
+    Route::get('/groups/create', [GroupController::class, 'create'])->name('groups.create');
+    Route::post('/groups', [GroupController::class, 'store'])->name('groups.store');
+    Route::get('/groups/{group}', [GroupController::class, 'show'])->name('groups.show');
+    Route::post('/groups/{group}/join', [GroupController::class, 'join'])->name('groups.join');
+    Route::post('/groups/{group}/leave', [GroupController::class, 'leave'])->name('groups.leave');
+
+    Route::get('/groups/{group}/posts', [GroupPostController::class, 'index'])->name('group_posts.index');
+    Route::post('/groups/{group}/posts', [GroupPostController::class, 'store'])->name('group_posts.store');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
