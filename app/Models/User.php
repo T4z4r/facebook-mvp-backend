b581d7aac365f93c5cd34f3976b6fdf7d5ba01e2
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 19:03:58 +0300

    fix; sanctum authentication issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,31 @@
 <?php
+
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password', 'avatar', 'bio'];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'avatar',
+        'bio',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
     public function posts()
     {
--- routes/api.php
+++ routes/api.php
@@ -1,47 +1,46 @@
 <?php
 
+use App\Http\Controllers\Api\ApiAuthController;
 use App\Http\Controllers\Api\ApiPostController;
 use App\Http\Controllers\Api\ApiFriendController;
 use App\Http\Controllers\Api\ApiMessageController;
 use App\Http\Controllers\Api\ApiGroupController;
-use App\Http\Controllers\Api\ApiNotificationController;
 use App\Http\Controllers\Api\ApiSearchController;
-use App\Http\Controllers\Api\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
 Route::post('/register', [ApiAuthController::class, 'register']);
 Route::post('/login', [ApiAuthController::class, 'login']);
+Route::get('/users/search', [ApiSearchController::class, 'search']);
 
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user', fn (Request $request) => $request->user());
     Route::post('/logout', [ApiAuthController::class, 'logout']);
 
+    // Posts
     Route::get('/posts', [ApiPostController::class, 'index']);
     Route::post('/posts', [ApiPostController::class, 'store']);
     Route::post('/posts/{post}/comments', [ApiPostController::class, 'comment']);
     Route::post('/posts/{post}/likes', [ApiPostController::class, 'like']);
     Route::delete('/posts/{post}/likes', [ApiPostController::class, 'unlike']);
 
+    // Friends
     Route::get('/friends', [ApiFriendController::class, 'index']);
     Route::get('/friends/requests', [ApiFriendController::class, 'requests']);
     Route::post('/friends', [ApiFriendController::class, 'store']);
     Route::put('/friends/{friend}/accept', [ApiFriendController::class, 'accept']);
 
+    // Messages
     Route::get('/messages', [ApiMessageController::class, 'index']);
     Route::post('/messages', [ApiMessageController::class, 'store']);
 
+    // Groups
     Route::get('/groups', [ApiGroupController::class, 'index']);
     Route::post('/groups', [ApiGroupController::class, 'store']);
     Route::get('/groups/{group}/posts', [ApiGroupController::class, 'posts']);
     Route::post('/groups/{group}/posts', [ApiGroupController::class, 'storePost']);
     Route::post('/groups/{group}/join', [ApiGroupController::class, 'join']);
     Route::post('/groups/{group}/leave', [ApiGroupController::class, 'leave']);
-
-    // Route::get('/notifications', [ApiNotificationController::class, 'index']);
-    // Route::post('/notifications/{notification}/read', [ApiNotificationController::class, 'markAsRead']);
 });
-
-Route::get('/users/search', [ApiSearchController::class, 'search']);
