3290d1e71186e58b2b5dac838d6c75eec060e8c5
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 09:15:30 +0300

    fix: group model

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -1,10 +1,25 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'creator_id'];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'group_user')
+                    ->withPivot('role');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(GroupPost::class);
+    }
 }
