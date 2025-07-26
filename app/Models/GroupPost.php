2355623bf2d635a2021d229961f71f201b243403
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 09:16:59 +0300

    feat: group user model

--- app/Models/GroupPost.php
+++ app/Models/GroupPost.php
@@ -1,10 +1,19 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class GroupPost extends Model
 {
-    //
+    protected $fillable = ['group_id', 'user_id', 'content', 'image_url'];
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
