344b09b0874105cf601d1326a6d88605a0bc6c74
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-26 09:16:20 +0300

    fix: group user model

--- app/Models/GroupUser.php
+++ app/Models/GroupUser.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class GroupUser extends Model
 {
-    //
+    protected $table = 'group_user';
+    protected $fillable = ['group_id', 'user_id', 'role'];
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
